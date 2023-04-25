01944ec5353044b4c25497711bebaa62a6ee33f9
Sumit Singh <Sumit18440@gmail.com>
2023-04-25 16:48:19 +0530

    cooki

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
 const app = express();
 
 dotenv.config({path: './config.env' }) ;
 require('./db/connection');
 
 app.use(express.static('public'));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({credentials: true, origin: 'https://sumit-auth.netlify.app'}));
