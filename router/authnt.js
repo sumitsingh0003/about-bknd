27363839b5ceed663cd9186691c76099e3f00bf6
Sumit Singh <Sumit18440@gmail.com>
2023-04-25 16:05:25 +0530

    Cookie

--- router/authnt.js
+++ router/authnt.js
@@ -66,11 +66,11 @@ router.post('/login', async (req, res) =>{
             const isMatch = await bcrypt.compare(password, userEmail.password);
             token = await userEmail.generateAuthToken();
             res.header("Access-Control-Allow-Origin", "https://sumit-auth.netlify.app");
-            res.cookie("jwtoken", token, {
+            const cook = res.cookie("jwtoken", token, {
                 expires: new Date(Date.now() + 86400000),  //24Hour k bad apne aap Log Out ho jayega (86400000 Milliseconds, 86400 Second, 1440 Minut, 24 Hours)
                 httpOnly: true
             })
-            console.log("Cookies", res.cookie("jwtoken", token))
+            console.log("Cookies", cook)
             if(!isMatch){
                res.status(400).json({ err : "Invalid Detailes" });
             } else {
