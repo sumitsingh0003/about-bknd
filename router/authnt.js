3a79ebd1b375854daf9f12fba2e2b818a05eec6a
Sumit Singh <Sumit18440@gmail.com>
2023-05-04 13:30:33 +0530

    cookie

--- router/authnt.js
+++ router/authnt.js
@@ -4,6 +4,7 @@ const multer  = require('multer');
 const authentications = require("../middleware/authentications");
 const router = express.Router();
 const fs = require('fs');
+const cookie = require('cookies');
 
 require("../db/connection");
 const User = require('../model/schema');
