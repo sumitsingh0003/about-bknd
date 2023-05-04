be7856791c11f65848db4cc6f515d1d08947e437
Sumit Singh <Sumit18440@gmail.com>
2023-05-04 11:52:01 +0530

    Changing

--- router/authnt.js
+++ router/authnt.js
@@ -75,6 +75,7 @@ router.post('/login', async (req, res) =>{
                 expires: new Date(Date.now() + 86400000),
                 secure: true,
                 httpOnly: true,
+                sameSite:"none"
             })
             // console.log(res.cookie("jwtoken", token), "Const Cookie Cookies")
 
